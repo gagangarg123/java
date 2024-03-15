49433bc30593896a68d6e1eab31812288ac7b0ad
gagangarg123 <garggagan700@gmail.com>
2024-03-15 19:11:46 +0530

    Sum of two numbers

---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Sum {
+    public static void main(String args[]){
+        Scanner gagan=new Scanner(System.in);
+        int a=gagan.nextInt();
+        int b=gagan.nextInt();
+        int sum=a+b;
+        System.out.print(sum);
+
+    }
+    
+}
