b068cc972245e2665006ab8b4f33b78226a56e0d
gagangarg123 <garggagan700@gmail.com>
2024-03-16 21:59:08 +0530

    patterns

---
+++ pattern.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class pattern {
+    public static void main(String args[]){
+System.out.println("Enter any number:");
+        Scanner gagan=new Scanner(System.in);
+int n=gagan.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+       
+    }
